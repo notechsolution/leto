903df9d8a95ae29b4102ed2cef4995717be08043
notechsolution <notech@126.com>
2016-07-27 07:17:22 +0800

    Leto - Lcharts - implement vue component using echarts based on https://github.com/Justineo/vue-echarts

--- src/App.vue
+++ src/App.vue
@@ -1,53 +1,107 @@
 <template>
-  <div id="app">
+  <div id="app" >
     <img class="logo" src="./assets/logo.png">
     <hello></hello>
     <p>
       Welcome to your Vue.js app!
     </p>
-    <p>
-      To get a better understanding of how this boilerplate works, check out
-      <a href="http://vuejs-templates.github.io/webpack" target="_blank">its documentation</a>.
-      It is also recommended to go through the docs for
-      <a href="http://webpack.github.io/" target="_blank">Webpack</a> and
-      <a href="http://vuejs.github.io/vue-loader/" target="_blank">vue-loader</a>.
-      If you have any issues with the setup, please file an issue at this boilerplate's
-      <a href="https://github.com/vuejs-templates/webpack" target="_blank">repository</a>.
-    </p>
-    <p>
-      You may also want to checkout
-      <a href="https://github.com/vuejs/vue-router/" target="_blank">vue-router</a> for routing and
-      <a href="https://github.com/vuejs/vuex/" target="_blank">vuex</a> for state management.
-    </p>
-
-    <div id='echarts-demo' style="width: 600px;height:400px;"></div>
+  </div>
+  <div id="charts">
+    <lcharts :options='polar'></lcharts>
+    <lcharts :options='bar' v-ref:bar></lcharts>
   </div>
 </template>
 
 <script>
 import Hello from './components/Hello'
+import Lcharts from './components/Lcharts.vue'
 import echarts from 'echarts'
+
+let asyncData = {
+  categories: ["衬衫","羊毛衫","雪纺衫","裤子","高跟鞋","袜子"],
+  data: [5, 20, 36, 10, 10, 20]
+}
 
 export default {
   components: {
-    Hello
+    Hello,
+    Lcharts
+  },
+  data: function () {
+    let data = []
+    for (let i = 0; i <= 360; i++) {
+      let t = i / 180 * Math.PI;
+      let r = Math.sin(2 * t) * Math.cos(2 * t);
+      data.push([r, i]);
+    }
+    return {
+      polar: {
+        title: {
+          text: '极坐标双数值轴'
+        },
+        legend: {
+          data: ['line']
+        },
+        polar: {
+          center: ['50%', '54%']
+        },
+        tooltip: {
+          trigger: 'axis',
+          axisPointer: {
+            type: 'cross'
+          }
+        },
+        angleAxis: {
+          type: 'value',
+          startAngle: 0
+        },
+        radiusAxis: {
+          min: 0
+        },
+        series: [
+          {
+            coordinateSystem: 'polar',
+            name: 'line',
+            type: 'line',
+            showSymbol: false,
+            data: data
+          }
+        ],
+        animationDuration: 2000
+      },
+      bar: {
+        title: {
+          text: '异步数据加载示例'
+        },
+        tooltip: {},
+        legend: {
+          data:['销量']
+        },
+        xAxis: {
+          data: []
+        },
+        yAxis: {},
+        series: [{
+          name: '销量',
+          type: 'bar',
+          data: []
+        }]
+      }
+    }
   },
   ready: function(){
-    var demoChart = echarts.init(document.getElementById('echarts-demo'));
-    demoChart.setOption({
-      title:{text:'Leto Demo Charts'},
-      tooltip:{},
-      xAxis:{
-        data:['衬衫','羊毛衫','雪纺衫','裤子','高跟鞋','袜子']
+// simulating async data from server
+    setTimeout(() => {
+      this.$refs.bar.mergeOptions({
+      xAxis: {
+        data: asyncData.categories
       },
-      yAxis :{},
-      series:[{
-        name:'销量',
-        type:'bar',
-        data:[5,16,36,12,31,20]
+      series: [{
+        name: '销量',
+        data: asyncData.data
       }]
-    }
-    );
+    })
+  }, 3000)
   }
 }
 </script>
@@ -67,6 +121,14 @@ body {
 #app {
   color: #2c3e50;
   margin-top: -100px;
+  max-width: 600px;
+  font-family: Source Sans Pro, Helvetica, sans-serif;
+  text-align: center;
+}
+
+#charts {
+  color: #2c3e50;
+  margin-top: 200px;
   max-width: 600px;
   font-family: Source Sans Pro, Helvetica, sans-serif;
   text-align: center;
